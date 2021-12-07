e35d4788f4b73310e7b3bea3df0b7db724c29cf3
Dhinesh <dhineshkumar.rajendran@ust.com>
2021-12-07 12:19:25 +0530

    first commit

---
+++ src/builderPatternDemo1/CakeBuilder.java
@@ -0,0 +1,18 @@
+package builderPatternDemo1;
+
+abstract class CakeBuilder {
+    protected Cake cake;
+    public Cake getCake(){
+        return cake;
+    }
+    public void newCake(){
+        cake = new Cake();
+    }
+    public abstract void buildCream();
+    public abstract void buildEggs();
+    public abstract void buildFlour();
+    public abstract void buildButter();
+    public abstract void buildMilk();
+    public abstract void buildSugar();
+}
+
---
+++ src/builderPatternDemo1/CakeBuilderDemo.java
@@ -0,0 +1,30 @@
+package builderPatternDemo1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CakeBuilderDemo {
+
+	public static void main(String[] args) throws IOException {
+		Chef chef = new Chef();
+		System.out.print("choose cake:");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String cakeType = br.readLine();
+		if (cakeType.equalsIgnoreCase("SwissRoll")) {
+			CakeBuilder swissRoll = new SwissRollCakeBuilder();
+			chef.setCakeBuilder(swissRoll);
+			chef.makeCake();
+			chef.getCake();
+
+		} else if (cakeType.equalsIgnoreCase("VictoriaSponge")) {
+			CakeBuilder victoriaSponge = new VictoriaSpongeCakeBuilder();
+			chef.setCakeBuilder(victoriaSponge);
+			chef.makeCake();
+			chef.getCake();
+		} else {
+			System.out.println("we don't have that cake in the menu");
+		}
+	}
+
+}
